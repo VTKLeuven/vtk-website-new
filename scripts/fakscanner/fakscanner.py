8a9b32f00a70e83aea98f018117cb410283f5acf
wiski <32271164+wiskiv1@users.noreply.github.com>
2026-08-18 01:40:47 +0200

    final changes

--- scripts/fakscanner/fakscanner.py
+++ scripts/fakscanner/fakscanner.py
@@ -51,7 +51,7 @@ except:
     exit()
 
 try:
-    requests.get(API_BASE)
+    requests.get(API_BASE, timeout=15)
     logger.info(f"Verbonden met {API_BASE}")
 except:
     screen.show("Geen verbinding", f"met {API_BASE}")
@@ -74,18 +74,18 @@ while True:
         )
     except requests.exceptions.RequestException as exc:
         screen.show("Geen verbinding", f"met {API_BASE}")
-        logger.info(f"Geen verbinding met de site: {exc}")
+        logger.error(f"Geen verbinding met de site: {exc}")
         time.sleep(10)
         continue
     
-    response = response.json()
-    # {'ok': True, 'counted': False, 'rNumber': 'r0939342', 'name': 'Witse Panneels', 'total': 1, 'points': 0, 'double': False, 'freeBeer': False, 'toNextBeer': 9, 'message': 'Al ingecheckt'}
-    
-    naam = ""
-    if response["name"]:
-        naam = response["name"]
-    else:
-        naam = response["rNumber"]
+    try:
+        response = response.json()
+        # {'ok': True, 'counted': False, 'rNumber': 'r0939342', 'name': 'Witse Panneels', 'total': 1, 'points': 0, 'double': False, 'freeBeer': False, 'toNextBeer': 9, 'message': 'Al ingecheckt'}
+    except:
+        screen.show("Website Error :(", " probeer opnieuw")
+        logger.error(f"Website error: {response}")
+        time.sleep(10)
+        continue
     
     if response["ok"] == False:
         logger.error(f"Scan failed: {response["error"]}")
@@ -93,17 +93,23 @@ while True:
         time.sleep(10)
         continue
     
+    naam = ""
+    if response["name"]:
+        naam = response["name"]
+    else:
+        naam = response["rNumber"]
+    
     if response["counted"] == False:
-        screen.show(response["message"], f"  {response["total"]} CheckIns  ")
+        screen.show(center_text(response["message"]), f"  {response["total"]} CheckIns  ")
         time.sleep(10)
         continue
     
     if response["double"]:
-        screen.show("Dubbele CheckIn!", center_text(response["name"]))
+        screen.show("Dubbele CheckIn!", center_text(naam))
         time.sleep(3)
     
     if response["freeBeer"]:
-        screen.show("  Gratis Pint!  ", center_text(response["name"]))
+        screen.show("  Gratis Pint!  ", center_text(naam))
         for i in range(3):
             for j in range(4):
                 time.sleep(0.1)
@@ -111,11 +117,11 @@ while True:
                 time.sleep(0.1)
                 screen.backlight(True)
             time.sleep(2)
-        time.sleep(7)
-        
-        
-        
-
+        time.sleep(10)
+        continue
+    
+    screen.show(center_text(naam), f"  {response["total"]} CheckIns  ")
+    time.sleep(7)
 
 # start main loop
 #   wait for cardscan
